7048bc7b553f5261361e5fd15f4af2593441fcf1
boseung2 <jungbs3726@naver.com>
2023-02-09 00:47:38 +0900

    chakra-ui 세팅

---
+++ src/app/components/PrimaryButton.tsx
@@ -0,0 +1,7 @@
+"use client";
+import { Button } from "@chakra-ui/react";
+import React from "react";
+
+export default function PrimaryButton() {
+  return <Button colorScheme="blue">Button</Button>;
+}
