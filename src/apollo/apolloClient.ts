e56088f96e9c51bcef4603258c69e05ced12f1be
boseung2 <jungbs3726@naver.com>
2023-02-10 03:03:40 +0900

    apollo port 번호 변경

--- src/apollo/apolloClient.ts
+++ src/apollo/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient } from "@apollo/client";
 import { createApolloCache } from "./createApolloCache";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  uri: "http://localhost:4000/graphql",
   cache: createApolloCache(),
 });
 
