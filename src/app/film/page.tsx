e7b1ea4ce37503ccd464280d7af6e22da58bf24d
boseung2 <jungbs3726@naver.com>
2023-02-14 02:58:45 +0900

    회원가입 mutation  추가

--- src/app/film/page.tsx
+++ src/app/film/page.tsx
@@ -4,7 +4,7 @@ import FilmList from "@/components/film/FilmList";
 import { Box, Heading } from "@chakra-ui/react";
 import React from "react";
 
-export default function page() {
+export default function Page() {
   return (
     <main>
       <Box>
