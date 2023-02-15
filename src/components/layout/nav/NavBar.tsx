e699a499bc2a16c982110ab00a71959b8324e6c0
boseung2 <jungbs3726@naver.com>
2023-02-16 00:36:47 +0900

    로그인 여부에 따라 navbar ui 변경

--- src/components/layout/nav/NavBar.tsx
+++ src/components/layout/nav/NavBar.tsx
@@ -5,11 +5,21 @@ import {
   Link,
   Stack,
   Button,
+  Avatar,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useMemo } from "react";
 import NextLink from "next/link";
+import { ColorModeSwitcher } from "../ColorModeSwitcher";
+import { useMeQuery } from "../../../generated/graphql";
 
 export default function NavBar() {
+  const accessToken = localStorage.getItem("access_token");
+  const { data } = useMeQuery({ skip: !accessToken });
+  const isLoggedIn = useMemo(() => {
+    if (accessToken) return data?.me?.id;
+    return false;
+  }, [accessToken, data?.me?.id]);
+
   return (
     <Box
       zIndex={10}
@@ -41,29 +51,44 @@ export default function NavBar() {
           </Link>
         </Flex>
 
-        <Stack justify="flex-end" direction="row" spacing={6}>
-          {/* <ColorModeSwitcher /> */}
-          <Button
-            fontSize="sm"
-            fontWeight={400}
-            variant="link"
-            as={NextLink}
-            href="/login"
-          >
-            로그인
-          </Button>
-          <Button
-            display={{ base: "none", md: "inline-flex" }}
-            fontSize="sm"
-            fontWeight={600}
-            href="/signup"
-            colorScheme="teal"
-            as={NextLink}
-          >
-            시작하기
-          </Button>
-        </Stack>
+        {isLoggedIn ? <LoggedInNavbarItem /> : <LoggedOutNavbarItem />}
       </Flex>
     </Box>
+  );
+}
+
+function LoggedInNavbarItem() {
+  return (
+    <Stack justify="flex-end" alignItems="center" direction="row" spacing={3}>
+      <ColorModeSwitcher />
+      <Avatar size="sm" />
+    </Stack>
+  );
+}
+
+function LoggedOutNavbarItem() {
+  return (
+    <Stack justify="flex-end" direction="row" spacing={6}>
+      <ColorModeSwitcher />
+      <Button
+        fontSize="sm"
+        fontWeight={400}
+        variant="link"
+        as={NextLink}
+        href="/login"
+      >
+        로그인
+      </Button>
+      <Button
+        display={{ base: "none", md: "inline-flex" }}
+        fontSize="sm"
+        fontWeight={600}
+        href="/signup"
+        colorScheme="teal"
+        as={NextLink}
+      >
+        시작하기
+      </Button>
+    </Stack>
   );
 }
